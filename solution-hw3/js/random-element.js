c79ce53c9a6fdaff8906d878d05cd4717ad88fca
lisayuli <lyuli@andrew.cmu.edu>
2023-09-22 17:49:48 -0400

    simplified js

--- solution-hw3/js/random-element.js
+++ solution-hw3/js/random-element.js
@@ -10,6 +10,7 @@ let price = document.querySelector("#price");
 glazeSelection.addEventListener("change", pickingGlaze);
 amountSelection.addEventListener("change", pickingAmount);
 
+//arrays storing display names and values
 let glazeOptionsNames = ["Keep Original", "Sugar Milk", "Vanilla Milk", "Double Chocolate"];
 let glazeOptionsValues = [0, 0, 0.5, 1.5];
 let amountOptionsNames = ["1", "3", "6", "12"];
@@ -31,30 +32,27 @@ for(let i = 0; i < amountOptionsNames.length; i++){
     amountSelection.add(amount);
 }
 
-
+//defining variables
 let basePrice = 2.49;
 let glazePrice = 0;
 let totalPrice = 0;
 let packSize = 1;
 
-
-
-function pickingAmount(){
-    packSize = parseInt(this.value);
-    displayPrice( updatePrice());
-}
+//changes the the glaze price value based on user's pick
 function pickingGlaze(){
     glazePrice = parseFloat(this.value);
-    displayPrice( updatePrice());
+    updatePrice();
 } 
 
-function updatePrice(){
-    totalPrice =  (basePrice + glazePrice) * packSize;
-    return totalPrice;
+//changes the pack size value base on user's pick
+function pickingAmount(){
+    packSize = parseInt(this.value);
+    updatePrice();
 }
 
-function displayPrice(totalPrice){
-    price.innerHTML = "$ " + totalPrice.toFixed(2);
-
+//updates the total price and displays it on the screen
+function updatePrice(){
+    totalPrice =  (basePrice + glazePrice) * packSize;
+    price.innerHTML = "$" + totalPrice.toFixed(2);
 }
 
