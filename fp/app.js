02dd40a98ff1ff1cc32181cfe5e566343599876f
lisayuli <lyuli@andrew.cmu.edu>
2023-12-05 11:55:30 -0500

    v1

---
+++ fp/app.js
@@ -0,0 +1,12 @@
+
+//using scroll reveal library to animate text to slide onto screen when scrolling to it
+let reveal = {
+    duration: "500",
+    delay: "300",
+    distance: "80px"
+};
+
+ScrollReveal().reveal(".revealContent", reveal);
+
+
+//making the dot of the 
